37e35a967c3b7532beb07c71d12c79ede6d34ea2
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2020-04-07 22:41:42 +0200

    Switching to Slurm.

--- job_script.sh
+++ job_script.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
 # Logging goes to directory sge_log
-#$ -o sge_log
-# Join stdout and stderr
-#$ -j y
-# Stay in current directory
-#$ -cwd
+#SBATCH -o slurm_log/%x-%J.log
 # Keep current environment variables
-#$ -V
+#SBATCH --export=ALL
 # Name of the script
-#$ -N MATLAB-example
+#SBATCH --job-name MATLAB-example
 # Allocate 4GB of RAM per core
-#$ -l h_vmem=4G
+#SBATCH --mem 4G
 # Maximal running time of 2 hours
-#$ -l h_rt=02:00:00
+#SBATCH --time 02:00:00
 # Allocate one Matlab license
-#$ -l license_matlab_r2016b=1
+#SBATCH -L matlab_r2016b:1
 
 module load matlab/r2016b-0
 
