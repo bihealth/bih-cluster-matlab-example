33ac03c6b1ae13fcdd5192b0be25c5a0ba2d185b
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2020-04-07 22:45:36 +0200

    Sourcing profile.d file.

--- job_script.sh
+++ job_script.sh
@@ -13,6 +13,10 @@
 # Allocate one Matlab license
 #SBATCH -L matlab_r2016b:1
 
+# This should be sourced here (in case we are submitting from the login
+# hosts which don't have this /etc/profile.d file).
+source /etc/profile.d/modules.sh
+
 module load matlab/r2016b-0
 
 matlab -r example
